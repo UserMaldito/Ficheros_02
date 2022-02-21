25d66089222b37fa076eb70fd394bce265643b83
Alumno <Alumno@DESKTOP-RD5LCFM>
2022-02-21 13:13:41 +0000

    Agregar archivos de proyecto.

---
+++ Ficheros_02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ficheros_02
+{
+	class Program
+	{
+		const string miSegundoFichero = "increible.txt";
+		static void Main(string[] args)
+		{
+
+
+
+
+		}
+	}
+	class Fichero
+	{
+
+	}
+}
